528dd14e591203417cd2fed815960857795f394e
salvoPernicano <salvo.pernicano@gmail.com>
2024-01-16 17:52:44 +0100

    feat difficolta'

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
+let settings = document.getElementById("difficulty");
+let contenitore = document.getElementById("board");
+function play(){
+    contenitore.innerHTML = "";
+    if (settings.value === "easy"){
 
-    let contenitore = document.getElementById("board");
-    for (let i = 1; i<= 81; i++){
-        let boxItem = document.createElement("div");
-        contenitore.append(boxItem)
-        boxItem.classList.add("item");
-        boxItem.textContent = i
-    }
+        for (let i = 1; i<= 100; i++){
+            let boxItem = document.createElement("div");
+            contenitore.append(boxItem)
+            boxItem.classList.add("item", "easy");
+            boxItem.textContent = i
+        }
+    } else if (settings.value === "medium"){
+        for (let i = 1; i<= 81; i++){
+            let boxItem = document.createElement("div");
+            contenitore.append(boxItem)
+            boxItem.classList.add("item", "medium");
+            boxItem.textContent = i
+        }
+    } else if (settings.value === "hard"){
+        for (let i = 1; i<= 49; i++){
+            let boxItem = document.createElement("div");
+            contenitore.append(boxItem)
+            boxItem.classList.add("item", "hard");
+            boxItem.textContent = i
+        }
+}
+}
+
